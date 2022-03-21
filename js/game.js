76c5fd51b3e684e0080c20a8863f80ad80b14590
leovasc5 <leovasc5@hotmail.com>
2022-03-20 23:46:47 -0300

    Changed some audios, created timer and part of the player box

--- js/game.js
+++ js/game.js
@@ -16,10 +16,40 @@ function choiceOther(){
     startGame.style.display = "none";
 }
 
+var timerOn = true;
+var time = 3;
+
+function fmtMSS(s){
+    return(s-(s%=60))/60+(9<s?':':':0')+s
+}
+
+function timedCount() {
+    if(time == -1){
+        timerOn = 0;
+        timer.innerHTML = "TEMPO<br>ESGOTADO"
+    }else{
+        timer.innerHTML = `TEMPO <br>${fmtMSS(time)}`;
+        time--;
+        setTimeout(timedCount, 1000);
+    }
+
+    
+}
+
+function startCount() {
+    if(timerOn) {
+        timedCount();
+    }
+}
+
+function getQuestionsToGame(){
+    
+}
+
 function start(){
     startGame.style.display = "None";
-    
     userImage.src = character.url_photo;
     userName.innerHTML = character.nome;
-
+    timedCount();
 }
+
