9acf8df252cd44e025bdf4fec2230166d3ddac6e
leovasc5 <leovasc5@hotmail.com>
2022-03-25 20:04:36 -0300

    SkipQuestion functions created

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-function choose(obj){
+function choose(obj) {
     cardsBox.style.display = "none";
     startGame.style.display = "block";
     characterPhoto.src = characters[obj]['url_photo']
@@ -11,7 +11,7 @@ function choose(obj){
     characterSelected.innerHTML = `VOCÊ ESCOLHEU: "${(characters[obj]['nome']).toUpperCase()}"`
 }
 
-function choiceOther(){
+function choiceOther() {
     cardsBox.style.display = "block";
     startGame.style.display = "none";
 }
@@ -19,15 +19,15 @@ function choiceOther(){
 var timerOn = true;
 var time = 900;
 
-function fmtMSS(s){
-    return(s-(s%=60))/60+(9<s?':':':0')+s
+function fmtMSS(s) {
+    return (s - (s %= 60)) / 60 + (9 < s ? ':' : ':0') + s
 }
 
 function timedCount() {
-    if(time == -1){
+    if (time == -1) {
         timerOn = 0;
         timer.innerHTML = "TEMPO<br>ESGOTADO"
-    }else{
+    } else {
         timer.innerHTML = `TEMPO <br>${fmtMSS(time)}`;
         time--;
         setTimeout(timedCount, 1000);
@@ -35,13 +35,13 @@ function timedCount() {
 }
 
 function startCount() {
-    if(timerOn) {
+    if (timerOn) {
         timedCount();
     }
 }
 
-function shuffleQuestions(array){
-    for(i=(array.length-1); i>0; i--){
+function shuffleQuestions(array) {
+    for (i = (array.length - 1); i > 0; i--) {
         rand = Math.floor(Math.random() * (i + 1));
         [array[i], array[rand]] = [array[rand], array[i]];
     }
@@ -49,11 +49,9 @@ function shuffleQuestions(array){
     return array;
 }
 
-function shuffleAnswers(resp, options){
+function shuffleAnswers(resp, options) {
     options.push(resp);
-
-    // console.log(array)
-    for(j=(options.length-1); j>0; j--){
+    for (j = (options.length - 1); j > 0; j--) {
         rand = Math.floor(Math.random() * (j + 1));
         [options[j], options[rand]] = [options[rand], options[j]];
     }
@@ -63,82 +61,88 @@ function shuffleAnswers(resp, options){
 
 var questions = shuffleQuestions(getQuestions());
 var pontos = 0;
+var questoesAntigas = [];
 
-function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
-  }
-  
-
-function reviewQuestion(element){
-    array_disable = document.getElementsByClassName(element.className);
-    for(m=0; m<=(array_disable.length-1); m++){
-        array_disable[m].disabled = true;
+function skipQuestion(element) {
+    questoesAntigas = document.getElementsByClassName(element.id.toString());
+    for (o = 0; o <= (questoesAntigas.length - 1); o++) {
+        questoesAntigas[o].style.display = 'none';
     }
 
-    for(l=0; l<=(questions.length-1); l++){
-        if(element.id == questions[l]['id']){
-            if(questions[l]['resp'] == element.textContent){
+    questions.shift();
+    novaQuestao = document.getElementsByClassName(questions[0]['id'].toString());
+    for (n = 0; n <= (novaQuestao.length - 1); n++) {
+        novaQuestao[n].style.display = "";
+    }
+}
+
+function reviewQuestion(element) {
+    array_disable = document.getElementsByClassName(element.className);
+    for (m = 0; m <= (array_disable.length - 1); m++) {
+        array_disable[m].disabled = true;
+    }
+    array_disable[array_disable.length-1].disabled = false;
+
+    for (l = 0; l <= (questions.length - 1); l++) {
+        if (element.id == questions[l]['id']) {
+            if (questions[l]['resp'] == element.textContent) {
                 element.style.backgroundColor = "green";
                 pontos += questions[l]['level'] * 25;
                 points.innerHTML = pontos;
-                sleep(500);
-            }else{
+            } else {
                 element.style.backgroundColor = "red";
-                sleep(500);
+                btns = document.getElementsByClassName(element.id.toString());
+                for(p = 0; p <= (btns.length - 1); p++){
+                    if (questions[l]['resp'] == btns[p].textContent) {
+                        btns[p].style.backgroundColor = "green";
+                        btns[p].style.opacity = "0.4";
+                        btns[p].style.transition = 'all 0.5s ease-in';
+                    }
+                }
             }
         }
     }
-    
-
-    questoesAntigas = document.getElementsByClassName(element.id.toString());
-    for(o=0; o<=(questoesAntigas.length-1); o++){
-        questoesAntigas[o].style.display = 'none';
-    }
-    
-    questions.shift();
-    novaQuestao = document.getElementsByClassName(questions[0]['id'].toString());
-    for(n=0; n<=(novaQuestao.length-1); n++){
-        novaQuestao[n].style.display = "";   
-    }
-    
 }
 
-
-function question(){
-
-    for(i=0; i<=(questions.length-1); i++){ //Correto
-    // for(i=0; i<=0; i++){ //Teste
+function question() {
+    for (i = 0; i <= (questions.length - 1); i++) {
         newDesc = document.createElement("span");
         newDesc.textContent = questions[i]['desc'];
         newDesc.setAttribute('class', questions[i]['id']);
-        if(i!=0){
-            newDesc.setAttribute('style', 'display: none');  
+        if (i != 0) {
+            newDesc.setAttribute('style', 'display: none');
         }
         separator.appendChild(newDesc);
 
         options = shuffleAnswers(questions[i]['resp'], questions[i]['options']);
-        
-        for(k=0; k<=(options.length-1); k++){
+
+        for (k = 0; k <= (options.length - 1); k++) {
             newButton = document.createElement("button");
             newButton.textContent = options[k];
             newButton.setAttribute('id', questions[i]['id']);
             newButton.setAttribute('class', questions[i]['id']);
             newButton.setAttribute('onclick', 'reviewQuestion(this)');
 
-            if(i!=0){
-                newButton.setAttribute('style', 'display: none');           
+            if (i != 0) {
+                newButton.setAttribute('style', 'display: none');
             }
             answerButtons.appendChild(newButton);
-        }   
+        }
+        newButtonContinue = document.createElement("button");
+        newButtonContinue.setAttribute('id', questions[i]['id']);
+        newButtonContinue.setAttribute('class', questions[i]['id']);
+        newButtonContinue.setAttribute('onclick', 'skipQuestion(this)');
+        newButtonContinue.setAttribute('disabled', 'true');
+        newButtonContinue.textContent = "Continuar";
+
+        if(i != 0){
+            newButtonContinue.setAttribute('style', 'display: none');
+        }
+        continueButton.appendChild(newButtonContinue);
     }
-    
 }
 
-function start(){
+function start() {
     startGame.style.display = "None";
 
     userImage.src = character.url_photo;
